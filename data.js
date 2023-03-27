60fcad6074c2d405736c8dc4262c5199024e12d9
damyeonse <128988323+damyeonse@users.noreply.github.com>
2023-03-27 18:16:40 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+let shopItemsData = [{
+    id:"lj",
+    name: "Leather Jacket",
+    price: 250,
+    desc: "Crafted from top-grain leather.",
+    img: "assets/leather-jacket.PNG"
+},
+{
+    id:"fs",
+    name: "Flannel Shirt",
+    price: 160,
+    desc: "Soft, organic cotton, great for layering.",
+    img: "assets/flannel-shirt.PNG"
+},
+{
+    id:"sd",
+    name: "Silk Dress",
+    price: 55,
+    desc: "Slinky, simple and versatile. Always chic, never fussy.",
+    img: "assets/silk-tee-dress.PNG"
+},
+{
+    id:"cj",
+    name: "Jogger",
+    price: 100,
+    desc: "Long lasting, three times as warm as wool.",
+    img: "assets/cashmere-jogger.PNG"
+},
+{
+    id:"cs",
+    name: "Crew Socks",
+    price: 20,
+    desc: "Exceptional breathability and rugged durability.",
+    img: "assets/crew-socks.PNG"
+},
+{
+    id:"as",
+    name: "Anklet Socks",
+    price: 15,
+    desc: "Certified Wool Standard (RWS) from farm to fiber.",
+    img: "assets/anklet-socks.PNG"
+},
+{
+    id:"menh",
+    name: "Modelo '89 Hi",
+    price: 160,
+    desc: "Made with bio-based vegan napa and recycled insole.",
+    img: "assets/modelo-89-hi.PNG"
+},
+{
+    id:"men",
+    name: "Modelo '89",
+    price: 150,
+    desc: "Super soft and all round comfort.",
+    img: "assets/modelo-89.PNG"
+}
+]
